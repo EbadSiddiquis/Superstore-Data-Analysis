9fa4c0773528b0f30666ff067ed5148a224aa76a
Ebad Siddiqui <143363284+EbadSiddiquis@users.noreply.github.com>
2024-10-15 18:04:19 -0500

    Update aggregate_insights.py

--- src/aggregate_insights.py
+++ src/aggregate_insights.py
@@ -6,14 +6,14 @@ df = pd.read_csv(superstore)
 
 # 4. Aggregation for Insights
 
-# Aggregate Sales, Profit, and Discount by Region
+# Aggregate Sales and Profit by Region
 agg_by_region = df.groupby('Region').agg({'Sales': 'sum', 'Profit': 'sum'})
 print("Sales, and Profit, by Region:\n", agg_by_region)
 
-# Aggregate Sales, Profit, and Discount by Category
-agg_by_category = df.groupby('Category').agg({'Sales': 'sum', 'Profit': 'sum', 'Discount': 'sum'})
-print("Sales, Profit, and Discount by Category:\n", agg_by_category)
+# Aggregate Sales and Profit by Category
+agg_by_category = df.groupby('Category').agg({'Sales': 'sum', 'Profit': 'sum'})
+print("Sales and Profit by Category:\n", agg_by_category)
 
-# Aggregate Sales, Profit, and Discount by Ship Mode
-agg_by_ship_mode = df.groupby('Ship Mode').agg({'Sales': 'sum', 'Profit': 'sum', 'Discount': 'sum'})
-print("Sales, Profit, and Discount by Ship Mode:\n", agg_by_ship_mode)
+# Aggregate Sales and Profit by Ship Mode
+agg_by_ship_mode = df.groupby('Ship Mode').agg({'Sales': 'sum', 'Profit': 'sum'})
+print("Sales and Profit by Ship Mode:\n", agg_by_ship_mode)
