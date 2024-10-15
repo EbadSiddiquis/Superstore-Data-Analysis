f0b2310cc8790633718ba1c11c4b516e53ac01ee
Ebad Siddiqui <143363284+EbadSiddiquis@users.noreply.github.com>
2024-10-15 18:25:02 -0500

    Update aggregate_insights.py

--- src/aggregate_insights.py
+++ src/aggregate_insights.py
@@ -14,6 +14,6 @@ print("Sales, and Profit, by Region:\n", agg_by_region)
 agg_by_category = df.groupby('Category').agg({'Sales': 'sum', 'Profit': 'sum', 'Discount': 'sum'})
 print("Sales, Profit, and Discount by Category:\n", agg_by_category)
 
-# Aggregate Sales, Profit, and Discount by Ship Mode
-agg_by_ship_mode = df.groupby('Ship Mode').agg({'Sales': 'sum', 'Profit': 'sum', 'Discount': 'sum'})
-print("Sales, Profit, and Discount by Ship Mode:\n", agg_by_ship_mode)
+# Aggregate Sales and Profit by Ship Mode
+agg_by_ship_mode = df.groupby('Ship Mode').agg({'Sales': 'sum', 'Profit': 'sum'})
+print("Sales and Profit by Ship Mode:\n", agg_by_ship_mode)
